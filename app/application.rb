d103ab466be32d97a7a3fb9a1e172b830bab82e1
tylerselldesign <tylerselldesign@gmail.com>
2019-01-22 20:48:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,5 +2,14 @@ class Application
   def call 
     resp = Rack::Response.new 
     
+    time = Time.now.hour 
+    
+    if time <= 12
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end
+    
+    resp.finish
   end
 end
